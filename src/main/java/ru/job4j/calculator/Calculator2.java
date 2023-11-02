bfa1a18853c3d9f16937427a6f3dccd3a988ce2e
Nikolya <nikolya.ilin.96@mail.ru>
2023-11-02 19:34:46 +0300

    3.1. Переназначение переменной [#232354 #457063]

---
+++ src/main/java/ru/job4j/calculator/Calculator2.java
@@ -0,0 +1,11 @@
+package ru.job4j.calculator;
+
+public class Calculator2 {
+    public static void main(String[] args) {
+        int size = 10;
+        size = 100;
+        int result = size - 5;
+        System.out.println(result);
+
+    }
+}
