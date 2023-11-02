8e7d24c41c61df934e1ed385d1b37b5ce39df4c9
Nikolya <nikolya.ilin.96@mail.ru>
2023-11-02 19:50:56 +0300

    6. Идеальный вес [#41584 #457115]

--- src/main/java/ru/job4j/calculator/Fit.java
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -2,22 +2,22 @@ package ru.job4j.calculator;
 
 public class Fit {
     public static double manWeight(short height1) {
-        double rsl = height1 * 1.15;
+        double rsl = (height1 - 100) * 1.15;
         return rsl;
     }
 
     public static double womanWeight(short height2) {
-        double rsl = height2 * 1.15;
+        double rsl = (height2 - 110) * 1.15;
         return rsl;
     }
 
     public static void main(String[] args) {
-        short height1 = 80;
-        short height2 = 60;
+        short height1 = 180;
+        short height2 = 160;
         double man = Fit.manWeight(height1);
         double woman = Fit.womanWeight(height2);
-        System.out.println("Woman 60 is " + woman);
-        System.out.println("Man 80 is " + man);
+        System.out.println("Woman 160 is " + woman);
+        System.out.println("Man 180 is " + man);
     }
 
 }
