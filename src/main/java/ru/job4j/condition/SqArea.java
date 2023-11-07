f5333c70c47c88752f5f9381d484ecbea835bb17
Nikolya <nikolya.ilin.96@mail.ru>
2023-11-07 22:47:47 +0300

    4. Тесты для стороны прямоугольника. [#159460]

--- src/main/java/ru/job4j/condition/SqArea.java
+++ src/main/java/ru/job4j/condition/SqArea.java
@@ -8,8 +8,26 @@ public class SqArea {
         return s;
     }
 
+    public static double square1(double p1, double k1) {
+        double  h1 = p1 / (2 * (k1 + 1));
+        double  i1 = h1 * k1;
+        double  s1 = i1 * h1;
+        return s1;
+    }
+
+    public static double square2(double p2, double k2) {
+        double  h2 = p2 / (2 * (k2 + 1));
+        double  i2 = h2 * k2;
+        double  s2 = i2 * h2;
+        return s2;
+    }
+
     public static void main(String[] args) {
-        double result1 = SqArea.square(6, 2);
-        System.out.println(" p = 6, k = 2, s = 2, real = " + result1);
+        double result = SqArea.square(6, 2);
+        System.out.println(" p = 6, k = 2, s = 2, real = " + result);
+        double result1 = SqArea.square(8, 4);
+        System.out.println(" p1 = 8, k1 = 4, s1 = 2.56, real1 = " + result1);
+        double result2 = SqArea.square(7, 9);
+        System.out.println(" p2 = 7, k2 = 9, s2 = 1.10, real2 = " + result2);
     }
 }
---
+++ src/test/java/ru/job4j/condition/SqAreaTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.condition;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class SqAreaTest {
+
+    @Test
+    void whenP6K2Square2()  {
+        int p = 6;
+        double k = 2;
+        int expected = 2;
+        double out = SqArea.square(p, k);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+        int p1 = 8;
+        double k1 = 4;
+        double expected1 = 2.56;
+        double out1 = SqArea.square(p1, k1);
+        assertThat(out1).isEqualTo(expected1, withPrecision(0.01));
+        int p2 = 7;
+        double k2 = 9;
+        double expected2 = 1.102;
+        double out2 = SqArea.square(p2, k2);
+        assertThat(out2).isEqualTo(expected2, withPrecision(0.01));
+     }
+    }
+
