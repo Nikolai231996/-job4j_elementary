39073614b9ecacd83e63947a7005ea49e6f0dca0
Nikolya <nikolya.ilin.96@mail.ru>
2023-11-07 18:34:45 +0300

    0. Что такое тестирование [#159431 #457365]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -17,10 +17,10 @@ public class Converter {
         float out = Converter.rubleToEuro(in);
         boolean euro = expected == out;
         System.out.println("70 rubles are " + euro + " euro.");
-        float in2 = 60;
-        float expected2 = 1;
-        float out2 = Converter.rubleToDollar(in2);
-        boolean dollar = expected2 == out2;
+        float in3 = 60;
+        float expected3 = 1;
+        float out3 = Converter.rubleToDollar(in3);
+        boolean dollar = expected3 == out3;
         System.out.println("60 rubles are " + dollar + " dollar.");
     }
 }
