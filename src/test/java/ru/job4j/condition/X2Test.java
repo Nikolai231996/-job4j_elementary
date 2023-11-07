6e4ef08ad5f5b29976ea18861c7dcef9dd7a7294
Nikolya <nikolya.ilin.96@mail.ru>
2023-11-07 21:46:09 +0300

    1.2. Входные данные в тесте [#297750]

---
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -0,0 +1,40 @@
+package ru.job4j.condition;
+
+import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class X2Test {
+
+    @Test
+    void whenA10B0C0X2Then40() {
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        int expected = 3;
+        int rsl = X2.calc(a, b, c, x);
+        assertThat(rsl).isEqualTo(expected);
+        int a1 = 0;
+        int b1 = 1;
+        int c1 = 1;
+        int x1 = 1;
+        int expected1 = 2;
+        int rsl1 = X2.calc(a1, b1, c1, x1);
+        assertThat(rsl1).isEqualTo(expected1);
+        int a2 = 1;
+        int b2 = 1;
+        int c2 = 0;
+        int x2 = 1;
+        int expected2 = 2;
+        int rsl2 = X2.calc(a2, b2, c2, x2);
+        assertThat(rsl2).isEqualTo(expected2);
+        int a3 = 1;
+        int b3 = 1;
+        int c3 = 1;
+        int x3 = 0;
+        int expected3 = 1;
+        int rsl3 = X2.calc(a3, b3, c3, x3);
+        assertThat(rsl3).isEqualTo(expected3);
+    }
+
+}
