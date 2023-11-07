d3a62bbb0e236b2ccc2f2300dac5cf51018ebaba
Nikolya <nikolya.ilin.96@mail.ru>
2023-11-07 18:25:11 +0300

    0. Что такое тестирование [#159431 #457365]12

---
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+public class Studentinfo {
+    public static void main(String[] args) {
+        System.out.println("Nikolya Ilin");
+        System.out.println("23.01.1997");
+    }
+}
+
+
