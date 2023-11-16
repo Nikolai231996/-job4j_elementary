f0f1941f3a9060f146ee41dfd96c6160d90bf298
Nikolya <nikolya.ilin.96@mail.ru>
2023-11-16 18:27:54 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -7,10 +7,6 @@ public class ArrayDefinition {
         names[1] = "Petr Butanov";
         names[2] = "Alex Vlasov";
         names[3] = "Egor Voronin";
-        String years = names[0];
-        String years1 = names[1];
-        String years2 = names[2];
-        String years3 = names[3];
         System.out.println(names[0]);
         System.out.println(names[1]);
         System.out.println(names[2]);
