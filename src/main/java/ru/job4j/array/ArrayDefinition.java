95316f3a20fa0d35cb966c7bd06a01b6f1c4b0fe
Nikolya <nikolya.ilin.96@mail.ru>
2023-11-16 17:37:18 +0300

    6.0.1. Объявление массива. [#3613]

---
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -0,0 +1,12 @@
+package ru.job4j.array;
+
+public class ArrayDefinition {
+    public static void main(String[] args) {
+        short[] agges = new short[10];
+        String[] surnames = new String[100500];
+        float[] prices = new float[40];
+        System.out.println(agges);
+        System.out.println(surnames);
+        System.out.println(prices);
+    }
+}
