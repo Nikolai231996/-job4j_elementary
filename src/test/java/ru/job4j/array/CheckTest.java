0e0dfa14553974d69ac7f81787f0549c916c69e0
Nikolya <nikolya.ilin.96@mail.ru>
2023-12-05 15:08:41 +0300

    6.3 Массив заполнен true или false  [#53857#461870]

--- src/test/java/ru/job4j/array/CheckTest.java
+++ src/test/java/ru/job4j/array/CheckTest.java
@@ -5,7 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class CheckTest {
     @Test
-    void whenDataMonoByTrueThenTrue() {
+    void whenDataMonoByTrueThenTrue1() {
         boolean[] data = new boolean[] {true, true, true};
         boolean result = Check.mono(data);
         assertThat(result).isTrue();
